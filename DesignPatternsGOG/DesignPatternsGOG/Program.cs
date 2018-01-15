08a37261a88d8fd859517324e9c9b0841f2a62f0
Róger Formagio Lopes <rogerformagiolopes@hotmail.com>
2018-01-15 20:41:42 -0200

    Initial Commit

---
+++ DesignPatternsGOG/DesignPatternsGOG/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatternsGOG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
