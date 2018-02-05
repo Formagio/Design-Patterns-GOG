8cc41db64be07f5f2781cde6dc59b86f187a17dc
Róger Formagio Lopes <rogerformagiolopes@hotmail.com>
2018-02-05 18:07:40 -0200

    Creational Patterns - Singleton

---
+++ DesignPatternsGOG/DesignPatternsGOG/CreationalPatterns/Singleton/Product/Server.cs
@@ -0,0 +1,14 @@
+﻿namespace DesignPatternsGOG.CreationalPatterns.Singleton.Product
+{
+    /// <summary>
+    /// Represents a server machine
+    /// </summary>
+    class Server
+    {
+        // Gets or sets server name
+        public string Name { get; set; }
+
+        // Gets or sets server IP address
+        public string IP { get; set; }
+    }
+}
--- DesignPatternsGOG/DesignPatternsGOG/Program.cs
+++ DesignPatternsGOG/DesignPatternsGOG/Program.cs
@@ -3,6 +3,7 @@ using AbstractFactory = DesignPatternsGOG.CreationalPatterns.AbstractFactory;
 using Builder = DesignPatternsGOG.CreationalPatterns.Builder;
 using FactoryMethod = DesignPatternsGOG.CreationalPatterns.FactoryMethod;
 using Prototype = DesignPatternsGOG.CreationalPatterns.Prototype;
+using Singleton = DesignPatternsGOG.CreationalPatterns.Singleton;
 
 namespace DesignPatternsGOG
 {
@@ -22,6 +23,9 @@ namespace DesignPatternsGOG
 
             Console.WriteLine("\nPrototype...");
             RunPrototype();
+
+            Console.WriteLine("\nSingleton...");
+            RunSingleton();
 
             // Wait for user input
             Console.ReadKey();
@@ -115,6 +119,33 @@ namespace DesignPatternsGOG
             var color1 = colormanager["red"].Clone() as Prototype.Product.Color;
             var color2 = colormanager["peace"].Clone() as Prototype.Product.Color;
             var color3 = colormanager["flame"].Clone() as Prototype.Product.Color;
+        }
+
+        /// <summary>
+        /// The singleton pattern ensures that only one object of a particular class is ever created. 
+        /// All further references to objects of the singleton class refer to the same underlying instance.
+        /// </summary>
+        static void RunSingleton()
+        {
+            var b1 = Singleton.LoadBalancer.GetLoadBalancer();
+            var b2 = Singleton.LoadBalancer.GetLoadBalancer();
+            var b3 = Singleton.LoadBalancer.GetLoadBalancer();
+            var b4 = Singleton.LoadBalancer.GetLoadBalancer();
+
+            // Confirm these are the same instance
+            if (b1 == b2 && b2 == b3 && b3 == b4)
+            {
+                Console.WriteLine("Same instance\n");
+            }
+
+            // Next, load balance 15 requests for a server
+            var balancer = Singleton.LoadBalancer.GetLoadBalancer();
+
+            for (int i = 0; i < 15; i++)
+            {
+                string serverName = balancer.NextServer.Name;
+                Console.WriteLine($"Dispatch request to: {serverName}");
+            }
         }
     }
 }
